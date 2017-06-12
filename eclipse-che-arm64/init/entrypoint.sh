b29860c21c7ec56397856b2ab0fdea5796764cd6
James Drummond <james@devcomb.com>
2017-06-12 05:22:38 +0000

    Add Eclipse Che Dockerfiles required for arm64

---
+++ eclipse-che-arm64/init/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+# Copyright (c) 2012-2016 Codenvy, S.A.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+# Contributors:
+#   Tyler Jewell - Initial Implementation
+#
+
+cp -rf /files/docs /copy
+
+# do not copy che.env if exist
+if [ ! -f  /copy/che.env ]; then
+    # if exist add addon env values to main env file.
+    if [ -f /etc/puppet/addon.env ]; then
+        cat /etc/puppet/addon.env >> /etc/puppet/manifests/che.env
+    fi
+    cp /etc/puppet/manifests/che.env /copy
+fi
